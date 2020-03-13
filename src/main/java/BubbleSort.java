dda1a53aa0c33236e3235cfebaf546a6c8299e60
s3785672 <s3785672@student.rmit.edu.au>
2020-03-13 17:08:14 +1100

    my first commit

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+
+    public static void main(int array[]) {
+
+        int n = array.length;
+
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+            if (array[j] > array[j+1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+
+
+
+        public void printArray(int sortedArr[]) {
+            
+                int p = sortedArr.length;
+
+
+        }
+
+
+    }
+
+}
